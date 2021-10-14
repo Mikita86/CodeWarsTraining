33ec9f5da65c75d7201a0e529b8a2cb569878e37
Mikita86 <mikithykoleg888@gmail.com>
2021-10-14 22:35:57 +0300

    tasks part 3

--- index.js
+++ index.js
@@ -81,3 +81,17 @@ function arrayDiff(a, b) {
     }
     return a;
 }
+
+
+function sumDigPow(a, b) {
+    eureka = [];
+    for (i = a; i <= b; i++) {
+        digits = String(i).split('');
+        if (i == digits.reduce(function(accumulator, currentValue, currentIndex) {
+                return accumulator + currentValue ** (currentIndex + 1);
+            }, 0)) {
+            eureka.push(i);
+        }
+    }
+    return eureka;
+}
